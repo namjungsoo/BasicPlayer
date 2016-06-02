917b2b5ff31ecbf033a1a4990c8b80db625fb553
남정수 <namjungsoo@gmail.com>
2016-06-03 05:56:36 +0900

    ndk 빌드 성공 #2

--- app/src/main/jni/Android.mk
+++ app/src/main/jni/Android.mk
@@ -1,5 +1,44 @@
 LOCAL_PATH := $(call my-dir)
 
+#BEGIN PREBUILT_SHARED_LIBRARY
+include $(CLEAR_VARS)
+LOCAL_MODULE:= libavcodec
+LOCAL_SRC_FILES:= ../lib/libavcodec.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+include $(PREBUILT_SHARED_LIBRARY)
+ 
+include $(CLEAR_VARS)
+LOCAL_MODULE:= libavformat
+LOCAL_SRC_FILES:= ../lib/libavformat.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+include $(PREBUILT_SHARED_LIBRARY)
+ 
+include $(CLEAR_VARS)   
+LOCAL_MODULE:= libswscale
+LOCAL_SRC_FILES:= ../lib/libswscale.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+include $(PREBUILT_SHARED_LIBRARY)
+ 
+include $(CLEAR_VARS)
+LOCAL_MODULE:= libavutil
+LOCAL_SRC_FILES:= ../lib/libavutil.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+include $(PREBUILT_SHARED_LIBRARY)
+ 
+include $(CLEAR_VARS)
+LOCAL_MODULE:= libavfilter
+LOCAL_SRC_FILES:= ../lib/libavfilter.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+include $(PREBUILT_SHARED_LIBRARY)
+ 
+include $(CLEAR_VARS)
+LOCAL_MODULE:= libwsresample
+LOCAL_SRC_FILES:= ../lib/libswresample.so
+LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)/include
+include $(PREBUILT_SHARED_LIBRARY)
+#END PREBUILT_SHARED_LIBRARY
+
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := libbasicplayer
@@ -18,4 +57,4 @@ LOCAL_ARM_MODE := arm
 
 include $(BUILD_SHARED_LIBRARY)
 
-$(call import-module,ffmpeg-3.0.2/android/arm,android/cpufeatures)
+#$(call import-module,ffmpeg-3.0.2/android/arm,android/cpufeatures)
