cc810ed4830d6e4853b182014141e5a6f44f9058
Jungsoo Nam <namjungsoo@gmail.com>
2016-06-17 16:46:18 +0900

    Toolbox 알파애니메이션 완료

--- app/src/main/java/com/duongame/basicplayer/activity/PlayerActivity.java
+++ app/src/main/java/com/duongame/basicplayer/activity/PlayerActivity.java
@@ -3,10 +3,15 @@ package com.duongame.basicplayer.activity;
 import android.app.Activity;
 import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v4.content.res.ResourcesCompat;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
+import android.view.ViewGroup;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.FrameLayout;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
 
 import com.duongame.basicplayer.MoviePlayView;
@@ -17,17 +22,23 @@ public class PlayerActivity extends Activity {
     private final static String TAG = "PlayerActivity";
 
     MoviePlayView playView;
+    ViewGroup toolBox;
+    class ToolBoxAnimation extends Animation {
 
+    }
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.layout_player);
         playView = (MoviePlayView) findViewById(R.id.moviePlay);
+        toolBox = (ViewGroup )findViewById(R.id.toolBox);
+
         if(playView != null) {
             playView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
+                    setToolBox(!FullscreenManager.isFullscreen());
                     FullscreenManager.setFullscreen(PlayerActivity.this, !FullscreenManager.isFullscreen());
                 }
             });
@@ -35,21 +46,41 @@ public class PlayerActivity extends Activity {
             playView.openFile(filename);
         }
 
-        final Button play = (Button)findViewById(R.id.play);
+        final ImageButton play = (ImageButton)findViewById(R.id.play);
         if(play != null) {// 일시정지를 시키자
             play.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if(playView.getPlaying())
+                    if(playView.getPlaying()) {
                         playView.pause();
-                    else
+                        play.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.pause, getApplicationContext().getTheme()));
+                    }
+                    else {
                         playView.resume();
+                        play.setImageDrawable(ResourcesCompat.getDrawable(getResources(), R.drawable.play, getApplicationContext().getTheme()));
+                    }
                 }
             });
         }
 
         applyNavigationBarHeight(true);
         FullscreenManager.setFullscreen(this, true);
+        setToolBox(true);
+    }
+
+    private void setToolBox(boolean newFullscreen) {
+        AlphaAnimation animation;
+        if(newFullscreen) {
+            animation = new AlphaAnimation(1.f, 0.0f);
+        }
+        else {
+            animation = new AlphaAnimation(0.0f, 1.f);
+        }
+        animation.setFillAfter(true);
+        animation.setFillEnabled(true);
+        animation.setDuration(300);
+        animation.setInterpolator(new AccelerateInterpolator());
+        toolBox.startAnimation(animation);
     }
 
     private void applyNavigationBarHeight(boolean portrait) {
