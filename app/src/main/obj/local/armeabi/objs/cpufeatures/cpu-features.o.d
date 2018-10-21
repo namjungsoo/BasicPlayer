cf51857dbc5fea6fb3b03e4222a2f2cac7d5f361
Jungsoo Nam <namjungsoo@gmail.com>
2018-10-21 21:52:23 +0900

    불필요한 파일 제거

--- app/src/main/obj/local/armeabi/objs/cpufeatures/cpu-features.o.d
+++
@@ -1,5 +0,0 @@
-obj/local/armeabi/objs/cpufeatures/cpu-features.o: \
- /Users/namjungsoo/android-ndk-r11c/sources/android/cpufeatures/cpu-features.c \
- /Users/namjungsoo/android-ndk-r11c/sources/android/cpufeatures/cpu-features.h
-
-/Users/namjungsoo/android-ndk-r11c/sources/android/cpufeatures/cpu-features.h:
