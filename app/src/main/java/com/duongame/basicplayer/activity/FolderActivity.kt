13a749d29377ce64882d5f3c340c375ed4f9725b
Jungsoo Nam <namjungsoo@gmail.com>
2018-10-27 23:17:13 +0900

    Kotlin 지원

---
+++ app/src/main/java/com/duongame/basicplayer/activity/FolderActivity.kt
@@ -0,0 +1,12 @@
+package com.duongame.basicplayer.activity
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import com.duongame.basicplayer.R
+
+class FolderActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
